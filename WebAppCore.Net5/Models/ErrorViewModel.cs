3fafd0a525351f1eb8b07024cca2dad29def8448
Mahekaru <Mahekaru@gmail.com>
2022-07-21 21:05:49 -0400

    Initial Commit 2

---
+++ WebAppCore.Net5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppCore.Net5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
